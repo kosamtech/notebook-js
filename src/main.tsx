e8efabc8cfa78a8c2eee5365769c1f61e9ef7e13
Kosam Omollo <hnkhosamomollo3@gmail.com>
2024-11-21 16:31:57 +0300

    [update] bind react with redux store with the provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { Provider } from "react-redux";
 import App from "./App.tsx";
+import { store } from "./state";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+);
