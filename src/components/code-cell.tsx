7068da644a56f5f6c5e211f6c28c27dc8739ece6
Kosam Omollo <hnkhosamomollo3@gmail.com>
2024-11-25 08:46:52 +0300

    [Refactor] updated code cell component to receive props from state rather than self

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,42 +1,68 @@
-import { useEffect, useState } from "react";
+import { FC, useEffect } from "react";
 import CodeEditor from "./code-editor";
 import Preview from "./preview";
-import bundle from "../bundler";
 import Resizable from "./resizable";
+import { Cell } from "../state";
+import { useActions } from "../hooks/use-actions";
+import { useTypedSelector } from "../hooks/use-typed-selector";
+import { useCumulativeCode } from "../hooks/use-cumulative-code";
+import "./code-cell.css";
 
-const CodeCell = () => {
-    const [code, setCode] = useState("");
-    const [input, setInput] = useState("");
-    const [err, setErr] = useState("");
+interface CodeCellProps {
+    cell: Cell;
+}
+
+const CodeCell: FC<CodeCellProps> = ({ cell }) => {
+    const { updateCell, createBundle } = useActions();
+    const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+    const cumulativeCode = useCumulativeCode(cell.id);
 
     useEffect(() => {
+        if (!bundle) {
+            createBundle({ cellId: cell.id, input: cumulativeCode });
+            return;
+        }
+
         const timer = setTimeout(async () => {
-            const output = await bundle(input);
-            setCode(output.code);
-            setErr(output.err);
+            createBundle({ cellId: cell.id, input: cumulativeCode });
         }, 1000);
 
         return () => {
             clearTimeout(timer);
         };
-    }, [input]);
+    }, [cell.id, cumulativeCode, createBundle]);
 
     return (
         <Resizable direction="vertical">
             <div
                 style={{
-                    height: "100%",
+                    height: "calc(100% - 10px)",
                     display: "flex",
                     flexDirection: "row",
                 }}
             >
                 <Resizable direction="horizontal">
                     <CodeEditor
-                        initialValue="const abc = 1234567890;"
-                        onChange={setInput}
+                        initialValue={cell.content}
+                        onChange={(value) =>
+                            updateCell({ id: cell.id, content: value })
+                        }
                     />
                 </Resizable>
-                <Preview code={code} err={err} />
+                <div className="progress-wrapper">
+                    {!bundle || bundle.loading ? (
+                        <div className="progress-cover">
+                            <progress
+                                className="progress is-small is-primary"
+                                max="100"
+                            >
+                                Loading
+                            </progress>
+                        </div>
+                    ) : (
+                        <Preview code={bundle.code} err={bundle.err} />
+                    )}
+                </div>
             </div>
         </Resizable>
     );
